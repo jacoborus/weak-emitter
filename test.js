67737a1dccb7dcb2a0590f6a8e1f99da633cab3c
Jacobo <jacobo@xarope.eu>
2016-04-14 02:14:29 +0200

    Build: add test for unsubscribe method

--- test.js
+++ test.js
@@ -7,10 +7,13 @@ test('hello world', t => {
   const emitter = ae()
   const obj = {}
   let control = 0
-  emitter.add(obj, () => ++control)
+  let unsubscribe = emitter.add(obj, () => ++control)
   emitter.trigger(obj)
-  t.is(control, 1)
+  t.is(control, 1, 'trigger')
   emitter.trigger(obj)
-  t.is(control, 2)
+  t.is(control, 2, 'trigger')
+  unsubscribe()
+  emitter.trigger(obj)
+  t.is(control, 2, 'unsubscribe')
   t.end()
 })
