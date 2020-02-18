335fb4164204cba5932e7946a4f1810f278703c6
Jacobo Tabernero <jacoborus@gmail.com>
2020-02-18 22:27:08 +0100

    Build: Version 5.1.0

--- lib/weak-emitter.d.ts
+++ lib/weak-emitter.d.ts
@@ -1,8 +1,12 @@
-declare type Handler = (...args: any[]) => void;
+declare type EventHandler = (...args: any[]) => void;
 declare type EventKey = string | object;
+export interface EventController {
+    off: EventHandler;
+    transfer: (context: object) => void;
+}
 export declare function weakEmitter(): {
-    on: (context: object, key: EventKey, handler: Handler) => void;
-    off: (context: object, key: EventKey, handler: Handler) => void;
+    on: (context: object, key: EventKey, handler: EventHandler) => EventController;
+    off: (context: object, key: EventKey, handler: EventHandler) => void;
     emit: (context: object, key: EventKey, ...args: any[]) => void;
 };
 export {};
