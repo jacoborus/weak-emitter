1425ed303add0d6c3810167af77d5f128edfbd93
Jacobo Tabernero <jacoborus@gmail.com>
2020-02-18 22:24:31 +0100

    Update: Add tests for eventController

--- src/weak-emitter.ts
+++ src/weak-emitter.ts
@@ -1,44 +1,61 @@
-type Handler = (...args: any[]) => void
-type EventStack = Set<Handler>
-type EventContext = Map<EventKey, EventStack>
+type EventHandler = (...args: any[]) => void
 type EventKey = string | object
+type EventStack = Set<EventHandler>
+type EventContext = Map<EventKey, EventStack>
+type Contexts = WeakMap<object, EventContext>
+
+export interface EventController {
+  off: EventHandler
+  transfer: (context: object) => void
+}
 
 export function weakEmitter () {
-  const contexts: WeakMap<object, EventContext> = new WeakMap()
+  const contexts: Contexts = new WeakMap()
 
   function newEvent (context: object): EventContext {
-    const event: EventContext = new Map()
-    contexts.set(context, event)
-    return event
+    const eventContext: EventContext = new Map()
+    contexts.set(context, eventContext)
+    return eventContext
   }
 
-  function newStack (event: Map<EventKey, any>, key: EventKey): EventStack {
+  function newStack (eventContext: Map<EventKey, any>, key: EventKey): EventStack {
     const stack: EventStack = new Set()
-    event.set(key, stack)
+    eventContext.set(key, stack)
     return stack
   }
 
-  function on (context: object, key: EventKey, handler: Handler): void {
-    const event = contexts.get(context) || newEvent(context)
-    const stack = event.get(key) || newStack(event, key)
+  function on (context: object, key: EventKey, handler: EventHandler): EventController {
+    let eventContext = contexts.get(context) || newEvent(context)
+    let stack = eventContext.get(key) || newStack(eventContext, key)
     stack.add(handler)
+    return {
+      off () { off(context, key, handler) },
+      transfer (destination: object) {
+        stack.delete(handler)
+        if (!stack.size) eventContext.delete(stack)
+        context = destination
+        eventContext = contexts.get(context) || newEvent(context)
+        stack = eventContext.get(key) || newStack(eventContext, key)
+        stack.add(handler)
+      }
+    }
   }
 
   function emit (context: object, key: EventKey, ...args: any[]): void {
-    const event = contexts.get(context)
-    if (!event) return
-    const stack = event.get(key)
+    const eventContext = contexts.get(context)
+    if (!eventContext) return
+    const stack = eventContext.get(key)
     if (!stack) return
-    stack.forEach((handler: Handler) => handler(...args))
+    stack.forEach((handler: EventHandler) => handler(...args))
   }
 
-  function off (context: object, key: EventKey, handler: Handler): void {
-    const event = contexts.get(context)
-    if (!event) return
-    const stack = event.get(key)
+  function off (context: object, key: EventKey, handler: EventHandler): void {
+    const eventContext = contexts.get(context)
+    if (!eventContext) return
+    const stack = eventContext.get(key)
     if (!stack) return
     stack.delete(handler)
-    if (!stack.size) event.delete(stack)
+    if (!stack.size) eventContext.delete(stack)
   }
 
   return { on, off, emit }
